007f69fc1a1b64cdfd19d846860e3fc45aa6d682
k8pai <thek8pai@gmail.com>
2023-01-26 18:18:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+
+function App() {
+	// make your changes here
+	return <h2>menu project setup</h2>
+}
+
+export default App
